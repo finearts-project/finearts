0044d58fea1c8c0f375c2647aba211cf64c50857
pradeepdurai <pradeepdurai66@gmail.com>
2020-12-25 16:36:45 +0530

    Changes

--- admin/add-edit-events.php
+++ admin/add-edit-events.php
@@ -60,7 +60,7 @@ function check_null($value)
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>Coordinators</h1>
+                    <h1>Events</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
