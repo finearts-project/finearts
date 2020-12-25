cea63b5c4e6346c7f6bae71a6904f078e8224a28
pradeepdurai <pradeepdurai66@gmail.com>
2020-12-25 16:31:24 +0530

    Changes

--- admin/view-events.php
+++ admin/view-events.php
@@ -12,7 +12,7 @@
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
-                    <h1>View Selection Fom List</h1>
+                    <h1>View Events List</h1>
                 </div>
                 <div class="col-sm-6">
                     <ol class="breadcrumb float-sm-right">
