75dda3674444dbe6755e96a1a9ec78dcab0bbd70
pradeepdurai <pradeepdurai66@gmail.com>
2020-12-27 18:41:23 +0530

    Changes

--- comman/conn.php
+++ comman/conn.php
@@ -3,7 +3,6 @@ error_reporting(E_ALL);
 ini_set("display_errors", 0);
 
 $GLOBALS['link'] = mysqli_connect('localhost', 'root', 'root', 'fine_arts')or die("Error Connecting to PHPmyadmin"); 
-// $GLOBALS['link'] = mysqli_connect('localhost', 'dbconfiguser', 'DBasdfjkl@!20', 'sakra_main')or die("Error Connecting to PHPmyadmin"); 
 $base_url = ''; 
 
 
